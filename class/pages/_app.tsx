a8c8cd4e95fd74720a6f0a0b7d2c9c7f3898a69d
daylee619 <110576332+daylee619@users.noreply.github.com>
2023-02-22 22:10:34 +0900

    save

--- class/pages/_app.tsx
+++ class/pages/_app.tsx
@@ -1,15 +1,19 @@
-import "../styles/globals.css";
-import { ApolloProvider, ApolloClient, InMemoryCache } from "@apollo/client";
+// import { ApolloProvider, ApolloClient, InMemoryCache } from "@apollo/client";
 import { AppProps } from "next/app";
+import Layout from "../src/components/commons/layout";
+import ApolloSetting from "../src/components/commons/apollo";
+import { Global } from "@emotion/react";
+import { globalStyles } from "../src/commons/styles/globalStyles";
 
-export default function App({ Component }: AppProps): JSX.Element {
-  const client = new ApolloClient({
-    uri: "http://practice.codebootcamp.co.kr/graphql",
-    cache: new InMemoryCache(),
-  });
+export default function App({ Component }: AppProps) {
   return (
-    <ApolloProvider client={client}>
-      <Component />;
-    </ApolloProvider>
+    <ApolloSetting>
+      <>
+        <Global styles={globalStyles} />
+        <Layout>
+          <Component />
+        </Layout>
+      </>
+    </ApolloSetting>
   );
 }
