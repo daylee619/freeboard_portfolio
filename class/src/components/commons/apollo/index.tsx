0434ee2b9bfbfcdfbd1e6a0b94f624b7ecf7c99d
daylee619 <110576332+daylee619@users.noreply.github.com>
2023-02-27 11:45:52 +0900

    infinite scroller

--- class/src/components/commons/apollo/index.tsx
+++ class/src/components/commons/apollo/index.tsx
@@ -3,6 +3,7 @@ import { ApolloProvider, ApolloClient, InMemoryCache } from "@apollo/client";
 interface IApolloSettingProps {
   children: JSX.Element;
 }
+
 export default function ApolloSetting(props: IApolloSettingProps) {
   const client = new ApolloClient({
     uri: "http://backendonline.codebootcamp.co.kr/graphql",
