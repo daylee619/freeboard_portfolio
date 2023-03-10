6ee185ca403bd9694eec4e6cb58d5bd47bce0120
daylee619 <110576332+daylee619@users.noreply.github.com>
2023-03-10 15:05:53 +0900

    구조분해할당

--- class/.eslintrc.js
+++ class/.eslintrc.js
@@ -18,5 +18,6 @@ module.exports = {
     "@typescript-eslint/no-misused-promises": "off",
     "@typescript-eslint/triple-slash-reference": "off",
     "react/display-name": "off",
+    "@typescript-eslint/naming-convention": "off",
   },
 };
