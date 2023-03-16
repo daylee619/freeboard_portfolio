672d08a8c2a8073ecdda8a2d5d393f09164567a4
daylee619 <110576332+daylee619@users.noreply.github.com>
2023-03-16 16:38:07 +0900

    save

--- class/src/components/commons/apollo/index.tsx
+++ class/src/components/commons/apollo/index.tsx
@@ -59,6 +59,7 @@ export default function ApolloSetting(props: IApolloSettingProps) {
     link: ApolloLink.from([uploadLink]),
     // cache: new InMemoryCache(),
     cache: GLOBAL_STATE, // 페이지 전환(_app.tsx 리렌더) 되어도 캐시 유지
+    connectToDevTools: true,
   });
 
   // prettier-ignore
