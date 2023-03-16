257ffddbbf65f8ec5f2022439add0bfe38faecb0
daylee619 <110576332+daylee619@users.noreply.github.com>
2023-03-16 15:10:30 +0900

    kakao map CSR

--- class/pages/_app.tsx
+++ class/pages/_app.tsx
@@ -8,6 +8,12 @@ import { RecoilRoot } from "recoil";
 
 export default function App({ Component }: AppProps) {
   return (
+    // { <Head> 모든 페이지에서 카카오맵을 다운로드 받으므로 비효율적임
+    //   <script
+    //     type="text/javascript"
+    //     src="//dapi.kakao.com/v2/maps/sdk.js?appkey=c614f32dcffc7d394e9014ffc620201c"
+    //   ></script>
+    // </Head> }
     <RecoilRoot>
       <ApolloSetting>
         <>
